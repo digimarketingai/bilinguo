ade139817a9c224482dc8eb3462f6914bdd62561
digimarketingai <digimarketingai50@gmail.com>
2026-01-31 12:07:43 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="bilinguo",
+    version="1.0.0",
+    py_modules=["bilinguo"],
+    install_requires=["gradio", "pandas", "gtts"],
+    author="digimarketingai",
+    description="Build bilingual termbases with audio in minutes"
+)
